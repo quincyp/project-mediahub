61ce2ba3ae25fc92f56be9d32b09093273458a54
TJ <tooraj@hotmail.com>
2020-12-17 14:57:16 -0800

    model set up initialized

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,32 @@
 module.exports = {
     movies: require("./movies"),
 }
+//require Mongoose
+const mongoose = require ("mongoose");
+
+const dbUrl = "mongodb://localhost:27017/movie"
+
+//connect
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
+.then(function(){
+    console.log("mongodb connected");
+})
+.catch(function(err){
+    console.log("Mongodb error");
+    console.log(err);
+          
+});
+
+mongoose.connection.on("disconnected", function(){
+    console.log("mongodb disconnected");
+});
+
+module.exports = {
+    Movie: require ("./Movie")
+};
+
