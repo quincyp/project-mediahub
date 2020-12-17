2007b3f8b0997b1bd6f9310a98e17265e9e934e9
qtpham <quincytpham@gmail.com>
2020-12-17 14:08:07 -0800

    added public middleware, home route, ejs view engine

--- server.js
+++ server.js
@@ -9,9 +9,18 @@ const app = express();
 /* ==== Configuration  ==== */
 const PORT = 3501;
 
+app.set("view engine", "ejs");
+
 /* ==== Middleware ==== */
+app.use(express.static(__dirname + '/public'));
+
 
 /* ==== Routes/Controllers  ==== */
+//Home Routes
+app.get("/", function(req, res) {
+    res.render("home");
+});
+
 
 /* ==== Server Listener  ==== */
 app.listen(PORT, function() {
