d0cfcd0122964651668cd606e5c75f6f273e6220
TJ <tooraj@hotmail.com>
2020-12-18 12:30:32 -0800

    added edit.ejs for comments and edited show.ejs

--- controllers/comments.js
+++ controllers/comments.js
@@ -5,39 +5,41 @@ const db = require("../models");
 
 // Index
 router.get("/", function (req, res) {
-	db.Comment.find({}, function (error, foundComments) {
-		if (error) return res.send(error);
+    db.Comment.find({}, function (error, foundComments) {
+        if (error) return res.send(error);
 
-		const context = {
-			comments: foundComments,
-		};
+        const context = {
+            comments: foundComments,
+        };
 
-		res.render("comments/index", context);
-	});
+        res.render("comments/index", context);
+    });
 });
 
 // New
 router.get("/new", function (req, res) {
-	db.Movie.find({}, function (err, foundMovies) {
-		if (err) return res.send(err);
+    db.Movie.find({}, function (err, foundMovies) {
+        if (err) return res.send(err);
 
-		const context = {
-			authors: foundMovies,
-		};
-		res.render("comments/new", context);
-	});
+        const context = {
+            movies: foundMovies,
+        };
+        res.render("comments/new", context);
+    });
 });
 
 // Show
-router.get ("/:id", function(req, res){
+router.get("/:id", function (req, res) {
     db.Comment
-    .findById(req.params.id)
-    .populate("comment")
-    .exec( function (err, foundComment) {
-        if (err) return res.send(err);
-        const context = { comment: foundComment};
-        res.render("comments/show, context");
-    });
+        .findById(req.params.id)
+        .populate("comment")
+        .exec(function (err, foundComment) {
+            if (err) return res.send(err);
+            const context = {
+                comment: foundComment
+            };
+            res.render("comments/show, context");
+        });
 });
 
 
@@ -55,6 +57,17 @@ router.post("/", function (req, res) {
         });
     });
 });
+
+// Edit 
+router.get("/:id/edit", function (req, res){
+    db.Comment.findById(req.params.id, function (err, foundComment){
+        if (err) return res.send(err);
+
+        const context = { comment: foundComment};
+        res.render("comments/edit", context);
+    });
+});
+
 
 
 
