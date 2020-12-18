b76ad4f2e1de0ccf399a6171d7dcdcfa12b1f5f4
TJ <tooraj@hotmail.com>
2020-12-18 11:42:43 -0800

    added new, show, create routes for comments

--- controllers/comments.js
+++ controllers/comments.js
@@ -3,7 +3,58 @@ const router = express.Router();
 
 const db = require("../models");
 
+// Index
+router.get("/", function (req, res) {
+	db.Comment.find({}, function (error, foundComments) {
+		if (error) return res.send(error);
 
+		const context = {
+			comments: foundComments,
+		};
+
+		res.render("comments/index", context);
+	});
+});
+
+// New
+router.get("/new", function (req, res) {
+	db.Movie.find({}, function (err, foundMovies) {
+		if (err) return res.send(err);
+
+		const context = {
+			authors: foundMovies,
+		};
+		res.render("comments/new", context);
+	});
+});
+
+// Show
+router.get ("/:id", function(req, res){
+    db.Comment
+    .findById(req.params.id)
+    .populate("comment")
+    .exec( function (err, foundComment) {
+        if (err) return res.send(err);
+        const context = { comment: foundComment};
+        res.render("comments/show, context");
+    });
+});
+
+
+// Create
+router.post("/", function (req, res) {
+
+    db.Comment.create(req.body, function (err, createdComment) {
+        if (err) return res.send(err);
+
+        db.Movie.findById(createdComment.movie).exec(function (err, foundComment) {
+            if (err) return res.send(err);
+            foundComment.movies.push(createdComment);
+            foundComment.save();
+            return res.redirect("/comments");
+        });
+    });
+});
 
 
 
