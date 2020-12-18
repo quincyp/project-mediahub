7e97754f9c02c3c62fdcdffebe1d7116fedfef2f
TJ <tooraj@hotmail.com>
2020-12-18 10:44:27 -0800

    added a model for comments and did a one to one connection on out schemas

--- models/Movie.js
+++ models/Movie.js
@@ -5,6 +5,7 @@ const movieSchema = new mongoose.Schema(
         name: { type: String, required: [true, "Name is needed"]},
         details: { type: String}, //REVIEW required: true, "Detail is needed"]
         year: { type: Number},
+        comments: [{type: mongoose.Schema.Types.ObjectId, ref: "Comment"}],
     },
     {
         timestamps: true
