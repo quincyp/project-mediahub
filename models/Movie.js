3053ee7b22037a80c72552b2afbc004fec4c5e36
qtpham <quincytpham@gmail.com>
2020-12-17 16:50:01 -0800

    Added year to movie database/schema, updated show.ejs  page to include year

--- models/Movie.js
+++ models/Movie.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const movieSchema = new mongoose.Schema(
     {
         name: { type: String, required: [true, "Name is needed"]},
-        details: { type: String} //REVIEW required: true, "Detail is needed"]
+        details: { type: String}, //REVIEW required: true, "Detail is needed"]
+        year: { type: Number},
     },
     {
         timestamps: true
