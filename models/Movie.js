9a6ffb040246f655a15990ce721f3c0f36c97171
TJ <tooraj@hotmail.com>
2020-12-21 16:03:48 -0800

    models reviewed, ERD.drawio revised

--- models/Movie.js
+++ models/Movie.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const movieSchema = new mongoose.Schema(
     {
         name: { type: String, required: [true, "Name is needed"]},
-        details: { type: String}, //REVIEW required: true, "Detail is needed"]
+        details: { type: String}, 
         year: { type: Number},
         comment: [{type: mongoose.Schema.Types.ObjectId, ref: "Comment"}],
     },
